c49c4e000b1039da08c43054c71065acb0637388
HafidurR <deifah126@gmail.com>
2022-01-07 20:43:34 +0700

    edit validasi user

--- models/user.js
+++ models/user.js
@@ -7,22 +7,70 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER
     },
     full_name: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Nama tidak boleh kosong'
+        }
+      }
     },
     email: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: {
+          msg: 'Email tidak valid'
+        },
+        notNull: {
+          msg: 'Email tidak boleh kosong'
+        }
+      },
+      unique: {
+        msg: 'Email sudah digunakan'
+      }
     },
     password: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Password tidak boleh kosong'
+        },
+        min: 6,
+        max: 10
+      }
     },
     gender: {
-      type: DataTypes.STRING
+      type: DataTypes.STRING,
+      values: ['male', 'female'],
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Gender tidak boleh kosong'
+        }
+      }
     },
     role: {
-      type: DataTypes.NUMBER
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Role tidak boleh kosong'
+        }
+      }
     },
     balance: {
-      type: DataTypes.NUMBER
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'Balance tidak boleh kosong'
+        },
+        isInt: true,
+        max: 100000000,
+        min: 0
+      }
     },
     createdAt: {
       allowNull: false,
@@ -31,6 +79,12 @@ module.exports = (sequelize, DataTypes) => {
     updatedAt: {
       allowNull: false,
       type: DataTypes.DATE
+    }
+  }, {
+    hooks: {
+      beforeValidate: (User, options) => {
+        User.balance = '0';
+      }
     }
   }, {
     tableName: 'Users'
