8f010a23da594cbbabd42473bc93ab19ba7e1395
HafidurR <deifah126@gmail.com>
2022-01-09 20:15:06 +0700

    edit migration for product

--- migrations/20211227041858-create-product.js
+++ migrations/20211227041858-create-product.js
@@ -9,16 +9,20 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       title: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       price: {
-        type: Sequelize.NUMBER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       stock: {
-        type: Sequelize.NUMBER
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
-      category_id: {
-        type: Sequelize.NUMBER
+      categoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
       },
       createdAt: {
         allowNull: false,
