d19079e87cf17db1d7c20753225fb9c2069e40a2
HafidurR <deifah126@gmail.com>
2022-01-14 20:58:30 +0700

    add create and get for controller product

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,5 @@
-const {
-  Product
-} = require('../models');
+const {Product} = require('../models');
+const rupiah    = require('../utils/formatMoney');
 
 class productController {
   static create = async (req, res) => {
@@ -10,28 +9,66 @@ class productController {
       price,
       categoryId
     } = req.body;
+    const {role} = req.user;
+    //return console.log(role);
 
-    await Product.create({
-      title,
-      stock,
-      price,
-      categoryId
-    }).then(result => {
-      return res.status(201).json({
+    if (role !== 'admin') {
+      return res.status(403).json({
+        status: 'Forbidden',
+        message: 'User unauthorized'
+      })
+    } else {
+      await Product.create({
+        title,
+        stock,
+        price,
+        categoryId
+      }).then(result => {
+        let data = {
+          id        : result.id,
+          title     : result.title,
+          price     : rupiah(result.price),
+          stock     : result.stock,
+          categoryId: result.categoryId,
+          updatedAt : result.updatedAt,
+          createdAt : result.createdAt
+        }
+        return res.status(201).json({
+          product: data
+        })
+      }).catch(error => {
+        const err = error.errors
+        const errorList = err.map(d => {
+          let obj = {}
+          obj[d.path] = d.message
+          return obj;
+        })
+        return res.status(400).json({
+          status: 'error',
+          message: errorList
+        });
+      });
+    }
+  }
+
+  static getAll = async (req, res) => {
+    let data = await Product.findAll()
+    data.forEach((data) => {
+     data.price = rupiah(data.price)
+    })
+    .then(result => {
+      return res.status(200).json({
         product: result
       })
-    }).catch(error => {
-      const err = error.errors
-      const errorList = err.map(d => {
-        let obj = {}
-        obj[d.path] = d.message
-        return obj;
-      })
+    })
+    .catch(error => {
       return res.status(400).json({
         status: 'error',
-        message: errorList
-      });
-    });
+        message: error.stack
+      })
+    })
   }
 
 }
+
+module.exports = productController;
