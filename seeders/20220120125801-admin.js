c82a68766caf68eab16e723421b81b1eed470c83
HafidurR <deifah126@gmail.com>
2022-01-20 20:00:06 +0700

    add seeder for rolw admin

---
+++ seeders/20220120125801-admin.js
@@ -0,0 +1,23 @@
+'use strict';
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      {
+        full_name: "Siblil Aula",
+        email: "sibli123@admin.com",
+        password: await bcrypt.hash("sibli123", 10),
+        gender: "male",
+        role: "admin",
+        balance: "50000000",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {});
+  }
+};
