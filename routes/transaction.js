1624b66316c824f5067e91e414428a1f467dcfc1
HafidurR <deifah126@gmail.com>
2022-01-22 00:17:13 +0700

    edit routes transaction

--- routes/transaction.js
+++ routes/transaction.js
@@ -3,6 +3,9 @@ const router              = express.Router();
 const transactionHandler  = require('../controller/transactionController');
 const verifyToken         = require('../middleware/verifyToken');
 
+router.get('/user', verifyToken, transactionHandler.getAll);
+router.get('/admin', verifyToken, transactionHandler.getAllAdmin);
+router.get('/:transactionId', verifyToken, transactionHandler.getTransaction);
 router.post('/',verifyToken, transactionHandler.create);
 
 module.exports = router;
